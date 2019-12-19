a9c1dc180532ef4bd006cf42431b4be436218027
Asada-Shino <1969681206@qq.com>
2019-12-19 14:37:48 +0800

    1

---
+++ Tank_Dome/Tank_Dome/Model/PlaySound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank_Dome.Model
+{
+    class PlaySound
+    {
+        private const int SND_SYNC = 0x0;
+        private const int SND_ASYNC = 0x1;
+        private const int SND_NODEFAULT = 0x2;
+        private const int SND_LOOP = 0x8;
+        private const int SND_NOSTOP = 0x10;
+
+        public static void Play(string file)
+        {
+            int flags = SND_ASYNC | SND_NODEFAULT;
+            sndPlaySound(file, flags);
+        }
+        [DllImport("winmm.dll")]
+        private extern static int sndPlaySound(string file, int uFlags);
+    }
+}
